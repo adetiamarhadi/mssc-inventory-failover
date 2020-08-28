938925e1df5fa928ab1310f36782118531d1774f
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2020-08-28 09:03:53 +0700

    init project

---
+++ src/test/java/com/github/adetiamarhadi/msscinventoryfailover/MsscInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adetiamarhadi.msscinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscInventoryFailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
